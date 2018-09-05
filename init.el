b72155df9a31b84f523bcf1da01867c1d29d066f
Justen Martin <jmart@the-coder.com>
2018-09-05 18:22:18 -0500

    configured csharp

--- init.el
+++ init.el
@@ -174,7 +174,8 @@ current buffer directory."
     (setq doom-themes-enable-bold t
 	  doom-themes-enable-italic t)
   :config
-    (load-theme 'doom-city-lights t)
+    ;;(load-theme 'doom-city-lights t)
+    (load-theme 'doom-spacegrey t)
     (doom-themes-visual-bell-config))
 
 
@@ -559,7 +560,50 @@ current buffer directory."
 	:after(flycheck company)
 	:hook (csharp-mode . omnisharp-mode)
 	:custom (omnisharp-debug t)
-	:init (add-to-list 'company-backends #'company-omnisharp))
+	:init (add-to-list 'company-backends #'company-omnisharp)
+		(which-key-add-major-mode-key-based-replacements 'csharp-mode
+			"<SPC> m" "dotnet"
+			"<SPC> mc" "compile project"
+			"<SPC> me" "solution errors"
+			"<SPC> mf" "find"
+			"<SPC> mfd" "definitions"
+			"<SPC> mfi" "implementations"
+			"<SPC> mfs" "symbols"
+			"<SPC> mfu" "usages"
+			"<SPC> mr" "refactor"
+			"<SPC> mrr" "rename"
+			"<SPC> ms" "server"
+			"<SPC> msc" "check status"
+			"<SPC> msi" "install"
+			"<SPC> msr" "reload solution"
+			"<SPC> mss" "start"
+			"<SPC> msS" "stop"
+			"<SPC> msv" "version info"
+			"<SPC> m=" "format"
+			"<SPC> m=f" "file"
+			"<SPC> m=r" "region"
+			)
+
+	(evil-leader/set-key-for-mode 'csharp-mode
+		"mc" 'recompile
+		"me" 'omnisharp-solution-errors
+		"mfd" 'omnisharp-go-to-definition
+		"mfi" 'omnisharp-find-implementations
+		"mfs" 'omnisharp-helm-find-symbols
+		"mfu" 'omnisharp-helm-find-usages
+
+		"mrr" 'omnisharp-rename-interactively
+
+		"msc" 'omnisharp-check-alive-status
+		"msi" 'omnisharp-install-server
+		"msr" 'omnisharp-reload-solution
+		"mss" 'omnisharp-start-omnisharp-server
+		"msS" 'omnisharp-stop-server
+
+		"m=f" 'omnisharp-code-format-file
+		"m=r" 'omnisharp-code-format-region
+		)
+	)
 
 (use-package terraform-mode)
 
