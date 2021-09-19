72160ffe6add27b5db926c156c2db66a0cc26f84
Justen Martin <jmart@the-coder.com>
2021-09-18 22:13:48 -0500

    theme improvements

--- modules/ui/theme.el
+++ modules/ui/theme.el
@@ -26,12 +26,14 @@
 		(doom-themes-enable-italic t)
 		(doom-themes-neotree-file-icons t)
 	:config
+	  ;;(load-theme 'doom-snazzy t)
+	  ;;(load-theme 'doom-ephemeral t)
 		;;(load-theme 'doom-nord t)
 		;;(load-theme 'doom-city-lights t)
 		;;(load-theme 'doom-dark+)
-		(load-theme 'doom-vibrant t)
+		;;(load-theme 'doom-vibrant t)
 		;; (load-theme 'doom-spacegrey t)
-		;; (load-theme 'doom-one t)
+		 (load-theme 'doom-one t)
 	  (doom-themes-neotree-config)
 	  (require 'doom-themes-ext-neotree)
 	  (doom-themes-visual-bell-config))
@@ -40,7 +42,11 @@
 		:hook (doom-load-theme . solaire-global-mode)
 			(which-key-init-buffer . solaire-mode)
 	)
-  )
 
+(use-package solaire-mode
+  :after (doom-themes)
+  :config (solaire-global-mode +1)
+  )
+)
 (provide 'ui-theme)
 ;;; theme.el ends here
